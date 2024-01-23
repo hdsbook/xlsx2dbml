711b326a0a00e6357f752a70856fe273247cbb36
HanDeCheng <Han.De.Cheng@ibm.com>
2024-01-23 23:29:35 +0800

    feat: 完成讀取xlsx，匯出dbml與相關報表的功能

---
+++ libraries/DBML2Report.js
@@ -0,0 +1,250 @@
+const fs = require('fs');
+const { Parser: DBMLParser } = require('@dbml/core');
+const path = require('path')
+const ExcelJS = require('exceljs');
+
+
+const DBML2Report = function (filePath) {
+    const that = this;
+
+    const dbmlContent = fs.readFileSync(filePath, 'utf-8');
+    try {
+        that.database = (new DBMLParser()).parse(dbmlContent, 'dbml');
+    } catch (error) {
+        const { start, end } = error.location;
+        const lines = dbmlContent.split('\n');
+        const errorLines = lines.slice(start.line - 1, end.line);
+        
+        console.error('DBML parse error:');
+        console.error(`錯誤檔案：${filePath}:`);
+        console.error(`錯誤行數：${start.line}:`);
+        console.error(errorLines.join('\n') + '\n');
+        console.error(`錯誤訊息：${error.message}`);
+    }
+}
+
+// 取得關聯對應 (欄位A -> 欄位B)
+DBML2Report.prototype.GetRefDic = function () {
+    const that = this;
+
+    let refDic = {};
+    that.database.schemas.every(schema => {
+        let refs = schema.refs;
+        refs.forEach(ref => {
+            let toFieldObj = ref.endpoints[0];
+            let fromFieldObj = ref.endpoints[1];
+
+            if (fromFieldObj.relation == '*' && toFieldObj.relation == '1') {
+                let toFieldName = toFieldObj.tableName + '.' + toFieldObj.fieldNames[0];
+                let fromFieldName = fromFieldObj.tableName + '.' + fromFieldObj.fieldNames[0]
+
+                if (!refDic[fromFieldName]) {
+                    refDic[fromFieldName] = [];
+                }
+                if (refDic[fromFieldName].indexOf(toFieldName) == -1) {
+                    refDic[fromFieldName].push(toFieldName)
+                }
+            }
+        })
+    })
+    return refDic;
+}
+
+// 將資料表關聯log出來
+DBML2Report.prototype.LogRelationships = function () {
+    const that = this;
+
+    that.database.schemas.every(schema => {
+        let refs = schema.refs;
+        refs.forEach(ref => {
+            let fromFieldObj = ref.endpoints[0];
+            let toFieldObj = ref.endpoints[1];
+
+            let fromFieldName = fromFieldObj.tableName + '.' + fromFieldObj.fieldNames[0];
+            let toFieldName = toFieldObj.tableName + '.' + toFieldObj.fieldNames[0]
+
+            let relation = '';
+            if (fromFieldObj.relation == '1' && toFieldObj.relation == '1') {
+                relation = '-';
+            } else {
+                if (toFieldObj.relation == '*') relation += '<';
+                if (fromFieldObj.relation == '*') relation += '>';
+            }
+            console.log(fromFieldName + ' ' + relation + ' ' + toFieldName);
+        })
+    })
+}
+
+// 讀取DBML，轉為匯出xlsx報表
+DBML2Report.prototype.DBML2Xlsx = function (xlsxName) {
+    const that = this;
+
+    const refDic = that.GetRefDic();
+
+    const sheets = [];
+    that.database.schemas.every(schema => {
+        let tables = schema.tables;
+        tables.forEach(table => {
+            const fields = [];
+            const fkSettings = {};
+
+            table.fields.forEach(field => {
+                //let schema = schema.name;
+                let fieldSettings = [];
+                if (field.pk) fieldSettings.push('PK');
+                if (field.not_null) fieldSettings.push('NN');
+
+                let fromFieldName = table.name + '.' + field.name;
+                let toFieldNames = refDic[fromFieldName] ? refDic[fromFieldName] : [];
+                if (toFieldNames.length > 0) {
+                    const toTableName = toFieldNames[0].split('.')[0];
+                    fkSettings[field.name] = {
+                        fkName: table.name + '_' + toTableName + '_FK',
+                        usedColumns: toFieldNames
+                    };
+                }
+
+                fields.push({
+                    tableName: table.name,
+                    tableNote: table.note,
+                    A: field.name,
+                    B: field.type.type_name,
+                    C: field.not_null ? 'TRUE' : 'FALSE',
+                    D: field.note,
+                    E: fieldSettings.join(', '),
+                    F: toFieldNames.join('\n')
+                });
+            })
+
+            sheets.push(fields);
+
+            // 如有主鍵，新增主鍵索引描述            
+            const pkField = table.fields.find(field => field.pk);
+            if (pkField) {
+                fields.push({}); // 新增空列
+
+                fields.push({
+                    A: "Field Name",
+                    B: "Index state",
+                    C: "Used columns",
+                    D: "Index expression"
+                });
+                fields.push({
+                    A: `${table.name}_PK`,
+                    B: "Primary Constraint",
+                    C: `${table.name}.${pkField.name}`,
+                    D: ""
+                });
+            }
+
+
+            if (Object.keys(fkSettings).length > 0) {
+                fields.push({}); // 新增空列
+                fields.push({
+                    A: "Field Name",
+                    B: "Used column",
+                    C: "Reference column"
+                });
+                Object.entries(fkSettings).forEach(([fieldName, { fkName, usedColumns }]) => {
+                    fields.push({
+                        A: fkName,
+                        B: fieldName,
+                        C: usedColumns.join('\n')
+                    });
+                })
+            }
+
+            fields.push({}); // 新增空列
+            fields.push({ A: `Table [${table.name}]${table.note}` });
+            // fields.push({}); // 新增空列
+            // fields.push({A: table.name});
+            // fields.push({A: table.note});
+        });
+    })
+
+    that.ExportXlsx(sheets, xlsxName);
+}
+
+
+DBML2Report.prototype.ExportXlsx = function (sheets, xlsxName) {
+
+    const workbook = new ExcelJS.Workbook();
+
+    sheets.forEach(fields => {
+        let sheetName = fields[0].tableNote;
+        if (sheetName.length > 27) {
+            sheetName = sheetName.substring(0, 27) + '...';
+        }
+        const worksheet = workbook.addWorksheet(sheetName);
+
+        // Define headers
+        worksheet.columns = [
+            { key: 'A', width: 35, header: 'Field Name' },
+            { key: 'B', width: 20, header: 'Data Type' },
+            { key: 'C', width: 15, header: 'Mandatory' },
+            { key: 'D', width: 30, header: 'Description' },
+            // { key: 'E', width: 15, header: '設定' },
+            // { key: 'F', width: 50, header: '關聯' },
+        ];
+        worksheet.addRows(fields);
+
+        // // 加上 border 樣式
+        // for (let row = 1; row <= fields.length; row++) {
+        //     for (let col = 1; col <= worksheet.columns.length; col++) {
+        //         const cell = worksheet.getCell(row, col);
+        //         if (col == 1 && !cell.value) {
+        //             break;
+        //         }
+
+        //         cell.border = {
+        //             top: { style: 'thin' },
+        //             left: { style: 'thin' },
+        //             bottom: { style: 'thin' },
+        //             right: { style: 'thin' },
+        //         };
+        //     }
+        // }
+    })
+
+    // 設定字體樣式
+    workbook.eachSheet((worksheet) => {
+        worksheet.eachRow((row) => {
+            let setBold = false;
+            row.eachCell((cell, colNumber) => {
+
+                // 將標題部分設為粗體
+                if (colNumber == 1 && cell.value == 'Field Name') {
+                    setBold = true;
+                }
+                cell.font = {
+                    size: 8,
+                    bold: setBold
+                };
+
+                // 加上 border 樣式
+                cell.border = {
+                    top: { style: 'thin' },
+                    left: { style: 'thin' },
+                    bottom: { style: 'thin' },
+                    right: { style: 'thin' },
+                };
+            });
+        });
+        worksheet.columns = [
+            { width: 30 },
+            { width: 20 },
+            { width: 20 },
+            { width: 30 },
+        ];
+    });
+
+
+
+    // Save workbook to file
+    workbook.xlsx.writeFile(path.resolve(__dirname, `./../${xlsxName}`))
+        .then(() => console.log(`報表已成功生成到 ${xlsxName} 檔案中。`));
+}
+
+
+
+module.exports = { DBML2Report };
---
+++ main.js
@@ -0,0 +1,140 @@
+const fs = require('fs');
+const ExcelJS = require('exceljs');
+const { DBML2Report } = require('./libraries/DBML2Report');
+
+// 讀取設定檔
+const config = JSON.parse(fs.readFileSync('./config.json'));
+const filterTables = config.filterTables;
+
+// input file path
+const inputFilePath = config['schemaFileName'] || "schema.xlsx"; 
+
+// output file paths
+const outputDBMLPath = './outputFiles/' + (config['outputDBMLFileName'] || "schema.dbml");
+const outputXlsxPath = './outputFiles/' + (config['outputXlsxFileName'] || "匯出報表.xlsx");
+const infoFilePath = './outputFiles/匯出資訊.txt';
+
+const workbook = new ExcelJS.Workbook();
+workbook.xlsx.readFile(inputFilePath).then(() => {
+    
+    // 讀取並解析 Excel 資料
+    const dataList = [];
+    workbook.eachSheet(sheet => {
+        sheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
+            if (rowNumber == 1) {
+                return; // 跳過第一列標題
+            }
+    
+            const rowData = {};
+            row.eachCell({ includeEmpty: false }, (cell, colNumber) => {
+                const headerCell = sheet.getCell(1, colNumber);
+                const headerName = headerCell.value;
+                rowData[headerName] = cell.text.toString().trim();
+            });
+            dataList.push(rowData);
+        });
+    })
+
+
+    // Excel 資料轉換為 tables 物件
+    let tables = {};
+    let maxNameLength = 0; // 紀錄最長欄位名稱長度(用於美化產出的DBML語法)
+    let maxTypeLength = 0; // 紀錄最長欄位型態長度(用於美化產出的DBML語法)
+    let hasFilterTables = filterTables.length > 0;
+    dataList.forEach(row => {
+        const tableName = row[config.titleTableName];
+        const note = row[config.titleChinese] ?? '';
+        const fieldName = row[config.titleEnglish] ?? '';
+        const dataType = row[config.titleDataType] ?? '';
+        const isPK = row[config.titlePrimaryKey] ?? '';
+        const isNN = row[config.titleNotNull] ?? '';
+        const defaultValue = row[config.titleDefault] ?? '';
+        const isUnique = row[config.titleUnique] ?? '';
+        const reference = row[config.titleReference] ?? '';
+        
+        // 篩選資料表
+        if (hasFilterTables && filterTables.indexOf(tableName) == -1) {
+            return; 
+        }
+        
+        if (!tables[tableName]) {
+            tables[tableName] = {
+                'tableNote': note,
+                'tableColumns': []
+            };
+        }
+        
+        const isColumnRows = tableName && fieldName && dataType;
+        if (isColumnRows) {
+            maxNameLength = Math.max(maxNameLength, fieldName.length);
+            maxTypeLength = Math.max(maxTypeLength, dataType.length);
+    
+            tables[tableName].tableColumns.push({
+                fieldName,
+                dataType,
+                note,
+                isPK,
+                isNN,
+                defaultValue,
+                isUnique,
+                reference
+            })
+        }
+    });
+
+    // 生成 DBML 語法
+    let dbmlContent = '';
+    Object.entries(tables).forEach(([tableName, { tableNote, tableColumns }]) => {
+        if (tableColumns.length == 0) {
+            return;
+        }
+
+        dbmlContent += `Table ${tableName}`;
+        if (tableNote) {
+            dbmlContent += ` [note: '${tableNote}']`;
+        }
+
+        dbmlContent += ` {\n`;
+        tableColumns.forEach(column => {
+            let spaces1 = ' '.repeat(maxNameLength - column.fieldName.length + 1);
+            let spaces2 = ' '.repeat(maxTypeLength - column.dataType.length + 1);
+
+            let settings = [];
+            if (column.isPK) settings.push('pk');
+            if (column.isNN) settings.push('not null');
+            if (column.defaultValue) settings.push(`default: '${column.defaultValue}'`);
+            if (column.note) settings.push(`note: '${column.note}'`);
+            if (column.reference) settings.push(`ref: > ${column.reference}`);
+            let settingsStr = settings.length > 0 ? `[${settings.join(', ')}]` : '';
+
+            dbmlContent += `\t${column.fieldName}${spaces1}${column.dataType}${spaces2}${settingsStr}\n`;
+        });
+        dbmlContent += `}\n\n`;
+    });
+
+    let dbmlProjectInfo =
+`Project TEST{
+    database_type: 'Oracle'
+    Note: '''
+        # TEST Schedule Database
+    '''
+}\n\n`;
+
+    // 寫入檔案 (生成DBML)
+    fs.writeFileSync(outputDBMLPath, dbmlProjectInfo + dbmlContent);
+    console.log(`DBML 語法已經成功生成到 ${outputDBMLPath} 檔案中。`);
+
+    // 匯出報表 (生成報表)
+    const converter = new DBML2Report(outputDBMLPath);
+    converter.DBML2Xlsx(outputXlsxPath);
+
+    // 匯出資料表清單
+    let infoContent = "";
+    let tableNames = Object.keys(tables);
+    let chineseTableNames = Object.entries(tables).map(([tableName, {tableColumns, tableNote}]) => tableNote);
+    infoContent += JSON.stringify(tableNames, null, '\t');
+    infoContent += "\n\n\n" + chineseTableNames.join('\n');
+    fs.writeFileSync(infoFilePath, infoContent, 'utf-8');
+});
+
+
